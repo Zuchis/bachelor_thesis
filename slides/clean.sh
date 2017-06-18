289b2ac5c341d1d18173a9b6ee8a7a55577b496a
Zuchis <liquimaster@gmail.com>
2017-06-18 16:51:44 +0100

    added the slides for the thesis defense

---
+++ slides/clean.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+rm svn.aux svn.toc svn.nav svn.out *.bib svn.snm svn.log *.blg 2> /dev/null || true
+
---
+++ slides/make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+COMP=pdflatex #-no-file-line-error
+BIB_COMP=bibtex
+MAIN=svn
+MAIN_BIB=svn
+
+${COMP}       ${MAIN}
+#${BIB_COMP}   ${MAIN}
+${COMP}       ${MAIN}
+${COMP}       ${MAIN}
